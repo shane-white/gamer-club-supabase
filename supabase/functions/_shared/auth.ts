76764a21b10d3231a9307a0361d5258cbab23543
mitchmiller08 <mitchmiller08@users.noreply.github.com>
2023-11-12 22:50:02 -0600

    Refactor veto

--- supabase/functions/_shared/auth.ts
+++ supabase/functions/_shared/auth.ts
@@ -3,8 +3,9 @@ import { json } from "sift";
 const admins = ["turbo_puns", "thecutout"]
 
 export async function adminWrapper(user: string, fn: (...args: any[]) => any) {
+  return async function (...args: any[]) {
     if (admins.includes(user)) {
-        return await fn();
+        return await fn(...args);
       }
       return json({
         type: 4,
@@ -12,4 +13,5 @@ export async function adminWrapper(user: string, fn: (...args: any[]) => any) {
           content: "You are not allowed to do that!",
         },
     });
+  }
 }
